5c3758c00839b56989169da0f641c56296317ca7
Joseph Kibithe <joekibithe2@gmail.com>
2022-06-20 15:17:38 +0300

    Wajakoya buds testnfts

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x1fb94A26c7d28b4b311f9D1C9645842fB0E6992f"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
